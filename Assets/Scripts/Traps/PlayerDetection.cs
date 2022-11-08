3a07848db0a378b785b2e84e3a310a4d4e74add5
Astronix8506 <113216692+Astronix8506@users.noreply.github.com>
2022-11-08 10:06:15 -0500

    Projectile trap
    Trap will shoot projectile of choice at an adjustable speed, direction, and tempo.  DOES NOT DO DAMAGE YET.  Added a few new sprites including .

---
+++ Assets/Scripts/Traps/PlayerDetection.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerDetection : MonoBehaviour
+{
+    public string tagTarget = "Player";     
+
+    public List<Collider2D> detectedObjs = new List<Collider2D> ();
+
+    void OnTriggerEnter2D(Collider2D collider)     //if an object with the tag "player" enter the collider, it adds collider to list
+    {
+        if(collider.gameObject.tag == tagTarget)
+        {
+            detectedObjs.Add (collider);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)       //if an object with the tag "player" exits the collider, it removes collider from list
+    {
+        if (collider.gameObject.tag == tagTarget)
+        {
+            detectedObjs.Remove(collider);
+        }
+    }
+}
