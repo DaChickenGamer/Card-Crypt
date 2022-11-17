8322645da05cfcbe99373958dc0fab663c6e1e4d
Astronix8506 <113216692+Astronix8506@users.noreply.github.com>
2022-11-17 16:32:16 -0500

    Fixed Slow Trap
    SlowTrap no longer freezes the melee class and now effects the range class

--- Assets/Scripts/Traps/slowTrap.cs
+++ Assets/Scripts/Traps/slowTrap.cs
@@ -6,18 +6,22 @@ public class slowTrap : MonoBehaviour
 {
     [SerializeField] private float movementReduction = 2.0f;
 
-    void OnTriggerStay2D(Collider2D collider)
+    void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.gameObject.tag == "Player" && (MeleePlayer.walkSpeed > 2 && MeleePlayer.walkSpeed < 10|| RangedPlayer.walkSpeed > 2 && RangedPlayer.walkSpeed < 10 || MagicPlayer.walkSpeed > 2 && MagicPlayer.walkSpeed < 10 ))   //collision detectores for activating trap and activating damage
+        if (collider.gameObject.tag == "Player" && ((MeleePlayer.walkSpeed > 2 && MeleePlayer.walkSpeed < 5) || (RangedPlayer.walkSpeed > 2 && RangedPlayer.walkSpeed < 5) || (MagicPlayer.walkSpeed > 2 && MagicPlayer.walkSpeed < 5 )))  //collision detectores for activating trap and activating damage
         {
-            MeleePlayer.walkSpeed /= movementReduction;
+            MeleePlayer.walkSpeed -= movementReduction;
+            RangedPlayer.walkSpeed -= movementReduction;
+            MagicPlayer.walkSpeed -= movementReduction;
         }
     }
     void OnTriggerExit2D(Collider2D collider)
     {
-        if (collider.gameObject.tag == "Player" && (MeleePlayer.walkSpeed < 20|| RangedPlayer.walkSpeed < 20 || MagicPlayer.walkSpeed < 20))   //collision detectores for activating trap and activating damage
+        if (collider.gameObject.tag == "Player" && (MeleePlayer.walkSpeed < 20 || RangedPlayer.walkSpeed < 20 || MagicPlayer.walkSpeed < 20))   //collision detectores for activating trap and activating damage
         {
-            MeleePlayer.walkSpeed *= movementReduction;
+            MeleePlayer.walkSpeed = 4;
+            RangedPlayer.walkSpeed = 4;
+            RangedPlayer.walkSpeed = 4;
         }
     }
 }
