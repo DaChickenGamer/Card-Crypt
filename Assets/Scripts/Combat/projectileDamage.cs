905112ff42f9cd0439b2845bb6b43d9a9071f43a
ChickenGamer <rjmichalski2006@gmail.com>
2022-12-06 10:49:14 -0500

    Projectile Destroy On Wall
    Made it so the projectile gets destroyed when it touches the wall.

--- Assets/Scripts/Combat/projectileDamage.cs
+++ Assets/Scripts/Combat/projectileDamage.cs
@@ -7,12 +7,18 @@ using UnityEngine;
 public class projectileDamage : MonoBehaviour
 {
         public void OnTriggerEnter2D(Collider2D hitInfo)
-    {
-        EnemyBehaviour enemy = hitInfo.GetComponent<EnemyBehaviour>();
-        if (enemy != null)
         {
-            enemy.TakeDamage((int)ShootScript.damage);
-            Destroy(gameObject);
+            EnemyBehaviour enemy = hitInfo.GetComponent<EnemyBehaviour>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage((int)ShootScript.damage);
+                Destroy(gameObject);
+            }
+            else if (hitInfo.gameObject.tag == "Walls")
+            {
+                Destroy(GameObject.FindWithTag("Projectile"));
+            }
         }
-    }
+  
+
 }
