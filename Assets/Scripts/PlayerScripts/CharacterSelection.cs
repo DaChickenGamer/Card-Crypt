53ee5d570850fb516adb205726442438a669976f
Lucifer8665 <76142200+Lucifer8665@users.noreply.github.com>
2023-01-05 09:56:41 -0500

    Update CharacterSelection.cs

--- Assets/Scripts/PlayerScripts/CharacterSelection.cs
+++ Assets/Scripts/PlayerScripts/CharacterSelection.cs
@@ -5,26 +5,19 @@ using UnityEngine;
 
 public class CharacterSelection : MonoBehaviour
 {
-    public int melee;
-    public static int meleePlayer;
-    public int ranged;
-    public static int rangedPlayer; 
-    public int magic;
-    public static int magicPlayer;
-    public void start()
-    {
-        melee = 0;
-        ranged = 0;
-        magic = 0;
-        meleePlayer = 0;
-        rangedPlayer = 0;
-        magicPlayer = 0;
+        public void Melee()
+        {
+            PlayerSpawner.PlayerVarible += 1;
+        ChangeScene.PlayerScene++;
     }
-
-    public void Update()
-    {
-        meleePlayer = melee;
-        rangedPlayer = ranged;
-        magicPlayer = magic;
+        public void Ranged()
+        {
+            PlayerSpawner.PlayerVarible += 2;
+        ChangeScene.PlayerScene++;
+    }
+        public void Magic()
+        {
+            PlayerSpawner.PlayerVarible += 3;
+        ChangeScene.PlayerScene++;
     }
 }
